07f7c22930cfd9593bf2efd24488bf0785382cad
breiner-r <breinerr006@gmail.com>
2025-04-18 16:00:33 -0500

    first commit

---
+++ src/main/java/com/sena/financeCosmeticosBellezaInfinita/FinanceCosmeticosBellezaInfinitaApplication.java
@@ -0,0 +1,13 @@
+package com.sena.financeCosmeticosBellezaInfinita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceCosmeticosBellezaInfinitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceCosmeticosBellezaInfinitaApplication.class, args);
+	}
+
+}
